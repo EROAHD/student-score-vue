46e0e56603c5d639786d892c87a2c347cc010d0c
demo <yrt6keo@protonmail.com>
2024-05-11 20:53:35 +0800

    修改DockerBuild.sh脚本

--- DockerBuild.sh
+++ DockerBuild.sh
@@ -1,8 +1,13 @@
 sudo apt install npm -y && \
+npm install && \
 npm run build && \
 cp -r ./dist DockerBuild/ && \
 cd DockerBuild && \
+# 尝试删除已有的同名镜像
 sudo docker rmi -f student_score_vue
+# 构建Docker镜像
 sudo docker build -t student_score_vue .
+# 尝试删除已存在的运行中的同名容器
 sudo docker rm -f student_score_vue
+# 运行容器
 sudo docker run -d -p 80:80 --name student_score_vue student_score_vue
